5e04648e6e4d2a9b65b7406799a0fc8a9012060d
Najeeb ullah <37581062+najeebjony@users.noreply.github.com>
2024-01-26 05:28:32 +0500

    upadte

---
+++ 09_computer_vision/02_webcam.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import cv2 as cv 
+
+# open the webcam using cv2
+import cv2
+
+# # Open the webcam
+# cap = cv2.VideoCapture(0)
+
+# # Check if the webcam is opened successfully
+# if not cap.isOpened():
+#     print("Error opening the webcam")
+# else:
+#     # Read and display frames from the webcam
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             print("Error reading frame from the webcam")
+#             break
+        
+#         cv2.imshow("Webcam", frame)
+        
+#         # Press 'q' to exit the loop
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+    
+    # Release the webcam and close the window
+    # cap.release()
+    # cv2.destroyAllWindows()
+
